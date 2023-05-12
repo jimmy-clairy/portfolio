82d638dcbddd783b90af6bec61067734c57b1be8
Jimmy Clairy <jimcl@hotmail.fr>
2023-05-12 10:58:21 +0200

    Add clock

--- js/script.js
+++ js/script.js
@@ -84,3 +84,33 @@ function lightMode() {
 
     modeTxt.textContent = "Dark Mode"
 }
+
+/** Clock */
+const hours = document.querySelector('.clock__hours')
+const minutes = document.querySelector('.clock__minutes')
+const seconds = document.querySelector('.clock__seconds')
+
+clock = () => {
+    let today = new Date()
+
+    let h = (today.getHours() % 12) + today.getMinutes() / 59 // 22 % 12 = 10pm
+    let m = today.getMinutes() // 0 - 59
+    let s = today.getSeconds() // 0 - 59
+
+    h *= 30 // 12 * 30 = 360deg
+    m *= 6 // 60 * 6 = 360deg
+    s *= 6 // 60 * 6 = 360deg
+
+    rotation(hours, h)
+    rotation(minutes, m)
+    rotation(seconds, s)
+
+    // call every second
+    setTimeout(clock, 1000)
+}
+
+rotation = (target, val) => {
+    target.style.transform = `rotate(${val}deg)`
+}
+
+window.onload = clock()
